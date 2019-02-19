72bf4bc953318e64ff68f546937a85969023ddff
Victor Chen <vichen@zynga.com>
2019-02-19 13:59:42 -0500

    Add support for URL param

--- src/background/background.js
+++ src/background/background.js
@@ -1,16 +1,32 @@
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
-    if (text && text.trim()) {
-      chrome.storage.local.get(['shortcuts'], function(result) {
-        const shortcuts = result.shortcuts || [];
-        console.log(shortcuts, text);
-        const shortcut = shortcuts.find(obj => obj.keyword === text.trim())
-        if (shortcut) {
-          const newURL = shortcut.url;
-          // chrome.tabs.create({ url: newURL });
-          chrome.tabs.update(null, {url: newURL});
-        }
-      });
+    if (!text || !text.trim()) {
+      return;
     }
+    chrome.storage.local.get(['shortcuts'], function(result) {
+      const shortcuts = result.shortcuts || [];
+
+      let shortcut = null;
+      let newUrl = null;
+
+      shortcut = shortcuts.find(obj => obj.keyword === text.trim())
+      if (shortcut) {
+        newUrl = shortcut.url;
+        chrome.tabs.update(null, {url: newUrl});
+        return;
+      }
+
+      // If exactly matching shortcut is not found, remove last segment of text (delimited by
+      // space) and try again, consider the last segment as URL param
+      const textSegs = text.split(' ');
+      text = textSegs.slice(0, -1).join(' ')
+      const suffix = textSegs.slice(-1).join(' ')
+
+      shortcut = shortcuts.find(obj => obj.keyword === text.trim())
+      if (shortcut) {
+        newUrl = shortcut.url + suffix;
+        chrome.tabs.update(null, {url: newUrl});
+      }
+    });
   });
